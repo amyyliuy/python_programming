3209ff0e797d2a862539309e95bf94b6736d4a69
amyyliuy <97140422+amyyliuy@users.noreply.github.com>
2022-01-06 18:47:48 +0800

    Delete oneLastTry.py

--- oneLastTry.py
+++
@@ -1,8 +0,0 @@
-howToDie = input("Choose your desired way to die: 1. IN A STORM (reply with 'storm') 2. THROWN OFF A CLIFF (reply with 'cliff') 3. SUICIDE ")
-
-if howToDie == "storm":
-  print("Congratulations, you survived! Yes, how can a storm kill you?")
-elif howToDie == "cliff":
-  print("Wow, you have so much courage! You didn't die, try sabotaging yourself during cliffdiving next time!")
-elif howToDie == "suicide":
-  print("WOW, HOW STUPID OF YOU! PLEASE DON'T KILL YOURSELF YOU DESERVE A HEALTHY LIFE!!!!!!!!")
